4ea5c0cef1d81083bac476431531112aad30f345
Lee McCuller <Lee.McCuller@gmail.com>
2021-10-20 10:17:26 -0500

    refactoring and cleanup

--- src/wavestate/epics/autocas/devices/NF3040/test/RUNDATA_T1_TST/epics/ioc.cmd
+++
@@ -1,3 +0,0 @@
-epics_ioc_registerRecordDeviceDriver(pdbbase)
-dbLoadRecords "/home/mcculler/local/home_sync/projects/ligoutils/YALL/controls/systems/TST3040/RUNDATA_T1_TST/epics/ioc.db"
-iocInit
--- src/wavestate/epics/autocas/devices/SR785/test/RUNDATA_T1_TST/epics/ioc.cmd
+++
@@ -1,3 +0,0 @@
-epics_ioc_registerRecordDeviceDriver(pdbbase)
-dbLoadRecords "/home/mcculler/local/home_sync/projects/ligoutils/YALL/controls/peripherals/SR785/test/RUNDATA_T1_TST/epics/ioc.db"
-iocInit
